26dd2fc1be6e0b6ffb980737fb533259b4ef0bd7
Koos85 <koosjoosten@gmail.com>
2025-07-07 15:04:38 +0200

    metricname

--- lib/check/per_drive_stats.py
+++ lib/check/per_drive_stats.py
@@ -19,8 +19,8 @@ async def check_per_drive_stats(
     for item in state['perDriveStatsTableEntry']:
         in_kb = item.pop('perDriveStatsTableInputKbytes')
         out_kb = item.pop('perDriveStatsTableOutputKbytes')
-        item['perDriveStatsTableInput'] = in_kb * 1000
-        item['perDriveStatsTableOutput'] = out_kb * 1000
+        item['perDriveStatsTableInputBytes'] = in_kb * 1000
+        item['perDriveStatsTableOutputBytes'] = out_kb * 1000
 
     return {
         'perDriveStats': state['perDriveStatsTableEntry']
